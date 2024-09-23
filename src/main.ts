bd3e1bdc89d2264deada12b8f81032324a503997
armandogreska <armandogreska@gmail.com>
2024-09-23 11:56:04 +0200

    prueba pre-commit ...

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
 
 bootstrapApplication(AppComponent, appConfig).catch(err => {
-  console.error('eee')
+
+
+
+
+
+
+
+
+
+
+
+
+  console.error('fff')
   return console.error(err)
 })
