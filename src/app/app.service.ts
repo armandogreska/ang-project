f1c3b4c0fac283647a8d7a09793c58b37467fa1e
armandogreska <armandogreska@gmail.com>
2025-04-22 12:29:32 +0200

    added environments ok

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,8 @@ export class AppService {
   private readonly http: HttpClient = inject(HttpClient)
 
   public getPosts(): Observable<object> {
-    return this.http.get<object>('http://localhost/?rest_route=/wp/v2/posts')
+    return this.http.get<object>(
+      environment.apiUrl + '?rest_route=/wp/v2/posts',
+    )
   }
 }
