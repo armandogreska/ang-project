ec8b730118cc71f3d5f34a5abf8bd7387eb780b4
armandogreska <armandogreska@gmail.com>
2025-05-05 19:01:54 +0200

    created search component. working!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { LoginComponent } from '../login/login.component'
+import { SearchComponent } from '../search/search.component'
 
 @Component({
   selector: 'app-header',
-  imports: [LoginComponent],
+  imports: [LoginComponent, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
