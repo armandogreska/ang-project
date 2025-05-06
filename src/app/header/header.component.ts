af32d2b8b6369728a8882eaf9c8d064e127c6160
armandogreska <armandogreska@gmail.com>
2025-05-06 11:26:16 +0200

    added cart component. working!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { CartComponent } from '../cart/cart.component'
 import { LoginComponent } from '../login/login.component'
 import { SearchComponent } from '../search/search.component'
 
 @Component({
   selector: 'app-header',
-  imports: [LoginComponent, SearchComponent],
+  imports: [LoginComponent, SearchComponent, CartComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
