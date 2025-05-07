6c4f5bd316d81fc17a58ea58dc8ce9741af3365b
armandogreska <armandogreska@gmail.com>
2025-05-07 19:50:34 +0200

    corrections code ok. working!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core'
-import { CartComponent } from '../cart/cart.component'
 import { LoginComponent } from '../login/login.component'
 import { SearchComponent } from '../search/search.component'
 
 @Component({
   selector: 'app-header',
-  imports: [LoginComponent, SearchComponent, CartComponent],
+  imports: [LoginComponent, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,57 +1,52 @@
 import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { BehaviorSubject, catchError, tap, throwError } from 'rxjs'
+import { inject, Injectable } from '@angular/core'
+import { catchError, tap, throwError } from 'rxjs'
 import { environment } from '../../environments/environment'
-import Token from '../models/Token'
-import User from '../models/User'
+import Token from '../interfaces/token.interface'
+import User from '../interfaces/user.interface'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private user = new BehaviorSubject<object | null>(null)
-  private token: string | null
-  // Init with null to filter out the first value in a guard!
-  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-    false,
-  )
+  // private user = new BehaviorSubject<object | null>(null)
+  // #token: string | null = null
+  readonly #httpClient = inject(HttpClient)
+  readonly #domain = environment.domain
+  readonly #api = environment.api
 
-  constructor(private _http: HttpClient) {
-    this.token = null
-  }
+  // private setItem(k: string, v: string): void {
+  //   localStorage.setItem(k, v)
+  // }
 
-  private setItem(k: string, v: string): void {
-    localStorage.setItem(k, v)
-  }
+  // private getItem(k: string): string | null {
+  //   return localStorage.getItem(k)
+  // }
 
-  private getItem(k: string): string | null {
-    return localStorage.getItem(k)
-  }
+  // private removeItem(k: string): void {
+  //   localStorage.removeItem(k)
+  // }
 
-  private removeItem(k: string): void {
-    localStorage.removeItem(k)
-  }
+  // private getIsAuthenticated(): boolean {
+  //   if (!this.token) {
+  //     return false
+  //   }
+  //   const payload = JSON.parse(atob(this.token.split('.')[1]))
+  //   const expirationDateMs: number = payload.exp * 1000
+  //   return Date.now() < expirationDateMs
+  // }
 
-  private getIsAuthenticated(): boolean {
-    if (!this.token) {
-      return false
-    }
-    const payload = JSON.parse(atob(this.token.split('.')[1]))
-    const expirationDateMs: number = payload.exp * 1000
-    return Date.now() < expirationDateMs
-  }
-
-  private logout(): void {
-    if (typeof this.token == 'string') {
-      this.removeItem(this.token)
-    }
-  }
+  // private logout(): void {
+  //   if (typeof this.token == 'string') {
+  //     this.removeItem(this.token)
+  //   }
+  // }
 
   public login(user: User) {
     // http://localhost/index.php/wp-json/jwt-auth/v1/token
 
-    return this._http
-      .post(environment.domain + environment.api + 'jwt-auth/v1/token', user)
+    return this.#httpClient
+      .post(this.#domain + this.#api + 'jwt-auth/v1/token', user)
       .pipe(
         tap((res: Token) => {
           if (res.token && res.token.length > 0) {
