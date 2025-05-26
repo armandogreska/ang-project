8778d32abb8fc08801c417459a2a5ea6d281c857
armandogreska <armandogreska@gmail.com>
2025-05-26 21:12:25 +0200

    integrated full backend ok. Working!

---
+++ .angular/cache/20.0.0-next.9/ang-project/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-SHXNNOUO.js";
+import "./chunk-RHNFKLDT.js";
+import "./chunk-YBKD5TML.js";
+import "./chunk-XXA435BE.js";
+import "./chunk-B56JISFQ.js";
+import "./chunk-FQIV5BWB.js";
+import "./chunk-F7VIALPV.js";
+import "./chunk-QLLR3KW5.js";
+import "./chunk-AKJ7V6S2.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
